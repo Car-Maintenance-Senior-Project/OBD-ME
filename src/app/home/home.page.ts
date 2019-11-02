110f4ff413b6c35ae62ef3343ae5b85983471198
Tyler Vredeveld <tjv37@students.calvin.edu>
2019-11-02 15:18:55 -0400

    Added settings page w/ routing + service start

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OBDParserService } from '../services/obdparser.service'
 
 @Component({
   selector: 'app-home',
@@ -7,13 +8,15 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  vin;
-  data2;
-  data3;
+  vin: string = "";
+  data2: string = "";
+  data3: string = "";
 
-  constructor() {}
+  constructor(private obdService: OBDParserService) {}
 
-  callGetVINFromParserService() {}
+  callGetVINFromParserService() {
+    this.vin = "test";
+  }
   callGet2FromParserService() {}
   callGet3FromParserService() {}
 
