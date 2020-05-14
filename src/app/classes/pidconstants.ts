2f3f44817beba863b7719fe2ece57d025b9c45d6
QuenMBar <Qmb2@students.calvin.edu>
2020-05-14 13:31:54 -0700

    More merge

--- src/app/classes/pidconstants.ts
+++ src/app/classes/pidconstants.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-// Constants for PIDs that are called for OBD
-=======
 // PIDs needed for various pieces of data - used to avoid confusing strings in code, easier to read
->>>>>>> 3824bed87b4a74024696005681edfc8d8d02b27a
 export class PIDConstants {
     public static Group1SupportedPIDs1: string = '01001\r';
     public static Group1SupportedPIDs2: string = '01021\r';
--- src/app/classes/storage-keys.ts
+++ src/app/classes/storage-keys.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-// Keys used to get and store data
-=======
 // keys for local storage to avoid using 'magic strings'
->>>>>>> 3824bed87b4a74024696005681edfc8d8d02b27a
 export class StorageKeys {
     public static LASTMAC: string = 'LASTMAC';
     public static CARPROFILES: string = 'CARPROFILES';
